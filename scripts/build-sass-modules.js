5b8356600e9593e18e6d7a7278fc6371959f28ac
thewebartisan7 <laraloop.com@gmail.com>
2023-04-18 06:17:58 +0200

    Refactor sass folder structure

--- scripts/build-sass-modules.js
+++ scripts/build-sass-modules.js
@@ -4,36 +4,61 @@ const allColorScales = require('../index');
 
 const outputDir = './sass';
 
-const forward = []
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
+  fs.mkdirSync(`${outputDir}/light`);
+  fs.mkdirSync(`${outputDir}/dark`);
+  fs.mkdirSync(`${outputDir}/overlay`);
+}
+
+const light = []
+const dark = []
+const overlay = []
 
 Object.entries(allColorScales).forEach(([colorScaleName, scale]) => {
+  const isDark = /DarkA?$/.test(colorScaleName);
+  const isOverlay = ['whiteA', 'blackA'].includes(colorScaleName);
+
   const scaleAssCssProperties = Object.entries(scale)
     .map(([name, value]) => `$${toKebabCase(name)}: ${value};`)
     .join('\n');
 
   const kebabCaseScaleName = toKebabCase(colorScaleName);
-
-  // const scaleAsCssFile = `$${kebabCaseScaleName}: (\n${scaleAssCssProperties}\n);`;
   const scaleAsCssFile = `${scaleAssCssProperties}`;
+  let fileName = `${kebabCaseScaleName}.scss`;
 
-  console.log('scaleAsCssFile', scaleAsCssFile)
-
-  // Add to @forward
-  forward.push(kebabCaseScaleName);
+  if (isOverlay) {
+    overlay.push(kebabCaseScaleName);
+    fileName = `overlay/${fileName}`;
+  } else {
+    if (isDark) {
+      dark.push(kebabCaseScaleName);
+      fileName = `dark/${fileName}`;
+    } else {
+      light.push(kebabCaseScaleName);
+      fileName = `light/${fileName}`;
+    }
+  }
 
   fs.writeFileSync(
-    path.join(outputDir, kebabCaseScaleName + '.scss'),
+    path.join(outputDir, fileName),
     scaleAsCssFile
   );
 });
 
-const indexContent = forward.map(f => {
-  return `@forward '${f}';`
-}).join('\n');
+fs.writeFileSync(
+  path.join(outputDir, 'light/index.scss'),
+  light.map(f => `@forward '${f}';`).join('\n')
+);
 
 fs.writeFileSync(
-  path.join(outputDir, 'index.scss'),
-  indexContent
+  path.join(outputDir, 'dark/index.scss'),
+  dark.map(f => `@forward '${f}';`).join('\n')
+);
+
+fs.writeFileSync(
+  path.join(outputDir, 'overlay/index.scss'),
+  overlay.map(f => `@forward '${f}';`).join('\n')
 );
 
 function toKebabCase(str) {
