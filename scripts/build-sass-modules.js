6fb24e635fcf590dc33b0b6302906f0101c35ef4
thewebartisan7 <laraloop.com@gmail.com>
2023-04-18 05:47:57 +0200

    Forward index

--- scripts/build-sass-modules.js
+++ scripts/build-sass-modules.js
@@ -4,18 +4,34 @@ const allColorScales = require('../index');
 
 const outputDir = './sass';
 
+const forward = []
+
 Object.entries(allColorScales).forEach(([colorScaleName, scale]) => {
   const scaleAssCssProperties = Object.entries(scale)
     .map(([name, value]) => `  ${toKebabCase(name)}: ${value},`)
     .join('\n');
 
-  const scaleAsCssFile = `$${toKebabCase(colorScaleName)}: (\n${scaleAssCssProperties}\n);`;
+  const kebabCaseScaleName = toKebabCase(colorScaleName);
+
+  const scaleAsCssFile = `$${kebabCaseScaleName}: (\n${scaleAssCssProperties}\n);`;
+
+  // Add to @forward
+  forward.push(kebabCaseScaleName);
 
   fs.writeFileSync(
-    path.join(outputDir, toKebabCase(colorScaleName) + '.scss'),
+    path.join(outputDir, kebabCaseScaleName + '.scss'),
     scaleAsCssFile
   );
 });
+
+const indexContent = forward.map(f => {
+  return `@forward '${f}';`
+}).join('\n');
+
+fs.writeFileSync(
+  path.join(outputDir, 'index.scss'),
+  indexContent
+);
 
 function toKebabCase(str) {
   return str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
